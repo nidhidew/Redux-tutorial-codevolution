cfaf2d003e0d04c0f838b78baba7bbd0f7d4e326
nidhidew <nidhicec97@gmail.com>
2024-10-12 14:53:20 +0530

    removed copied directory of repo

--- Redux-tutorial-codevolution/tutorial_redux/Code/react-redux-demo/src/redux/index.js
+++
@@ -1,2 +0,0 @@
-export { buyCake } from "./cakes/cakeActions";
-
--- Redux-tutorial-codevolution/tutorial_redux/Code/react-redux-demo/src/redux/store.js
+++
@@ -1,6 +0,0 @@
-import { legacy_createStore as createStore} from 'redux'
-import { cakeReducer } from './cakes/cakeReducer'
-
-const store = createStore(cakeReducer)
-
-export default store
